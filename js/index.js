fedd55b4c04cb1e78a293c3d9b41097b545e05a2
Xavier Trinidad <xmtrinidad@gmail.com>
2017-08-23 22:25:27 -0700

    Section 1.0 complete, mobile responsive, broke out some second level ul elements into two cols

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ $(".objective_title").on("click", function(){
     if ($(window).width() >= 768) {
         return;
     }
+    $('i', this).toggleClass("arr_down");
     $(this).next().toggleClass("show_info");
 });
 
